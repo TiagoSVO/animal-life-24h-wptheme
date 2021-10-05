bd79ba53a6de1530b2a275ad089c9caf90f2d831
Tiago da Silva Valeriano de Oliveira <tiago.oliveira@ifb.edu.br>
2021-10-05 07:25:59 -0300

    [FEATURE] Adição do gmap na page-home e ajustes no layout e css

--- page-home.php
+++ page-home.php
@@ -4,6 +4,7 @@
     <?php get_template_part('template-parts/content', 'main-carousel') ?>
     <?php get_template_part('template-parts/content', 'about') ?>
     <?php get_template_part('template-parts/content', 'services') ?>
+    <?php get_template_part('template-parts/content', 'gmap') ?>
   </article>
 </main>
 <?php get_footer();?>
